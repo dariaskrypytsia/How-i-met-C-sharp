2cd1bc071030f34c20f1e25354e39110fabc99da
dariaskrypytsia <dariaskrypytsia@gmail.com>
2026-09-17 11:08:48 +0200

    test number two

--- Program.cs
+++ Program.cs
@@ -11,4 +11,13 @@ familyBusinessManagment.Enroll(fredo); // no place left for Fredo
 familyBusinessManagment.Enroll(michael); //already in
 
 Console.WriteLine($":Michael's courses count: {michael.Courses.Count}"); 
-Console.WriteLine($"Family business students count: {familyBusinessManagment.Students.Count}"); 
+Console.WriteLine($"Family business managment students count: {familyBusinessManagment.Students.Count}"); 
+
+familyBusinessManagment.Remove(sonny);
+Console.WriteLine($"Sonny's courses after leaving: {sonny.Courses.Count}");
+
+familyBusinessManagment.Remove(sonny); 
+
+// now there's room, so Fredo can join
+familyBusinessManagment.Enroll(fredo);
+Console.WriteLine($"Family business managment students count after Fredo joined: {familyBusinessManagment.Students.Count}");
